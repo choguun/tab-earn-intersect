2a095b5cb9beefe8818c6af0f0d64ae54e9c13d2
choguun <29241145+choguun@users.noreply.github.com>
2024-09-20 14:29:34 +0800

    Update supportedChains.ts

--- packages/client/src/mud/supportedChains.ts
+++ packages/client/src/mud/supportedChains.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /*
  * The supported chains.
  * By default, there are only two chains here:
@@ -17,4 +18,31 @@ import { MUDChain, mudFoundry, redstone, garnet } from "@latticexyz/common/chain
  * See https://mud.dev/guides/hello-world/add-chain-client
  * for instructions on how to add networks.
  */
-export const supportedChains: MUDChain[] = [mudFoundry, redstone, garnet];
+
+
+export const interact: any = {
+    id: 1612, // Your custom chain ID
+    name: 'InteractTestnet',
+    // network: 'my-custom-network',
+    nativeCurrency: {
+      name: 'Perl Token',
+      symbol: 'Perl',
+      decimals: 18,
+    },
+    rpcUrls: {
+      default: {
+        http: ['https://testnet-pearl-c612f.avax-test.network/ext/bc/CcXVATAg76vM849mrPoTigwp48qhFiN9WCa51DBQXNGkBKZw7/rpc?token=3296aa3e491dd5d366815601cc95be7275cd293486b09fe082619750d7b38587'],
+      },
+      public: {
+        http: ['https://testnet-pearl-c612f.avax-test.network/ext/bc/CcXVATAg76vM849mrPoTigwp48qhFiN9WCa51DBQXNGkBKZw7/rpc?token=3296aa3e491dd5d366815601cc95be7275cd293486b09fe082619750d7b38587'],
+      },
+    },
+    blockExplorers: {
+      default: {
+        name: 'InteractExplorer',
+        url: 'https://subnets-test.avax.network/intersect',
+      },
+    },
+};
+
+export const supportedChains: MUDChain[] = [mudFoundry, redstone, garnet, interact];
